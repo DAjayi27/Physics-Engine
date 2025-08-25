96d7479db4bfaa193d510dd2b841db3f25ca8748
DAjayi <dr716500@dal.ca>
2025-08-25 15:28:59 -0300

    Testing ui changes

---
+++ src/ui/ui_popup.cpp
@@ -0,0 +1,5 @@
+//
+// Created by AJYDAN on 25/08/2025.
+//
+
+#include "ui_popup.h"
---
+++ src/ui/ui_popup.h
@@ -0,0 +1,16 @@
+//
+// Created by AJYDAN on 25/08/2025.
+//
+
+#ifndef POPUP_H
+#define POPUP_H
+
+
+
+class ui_popup {
+
+};
+
+
+
+#endif //POPUP_H
